881c1902df1e7b353d6a4e17694a8f7dafbf8665
Joe <Joseph.Lucas-1@ou.edu>
2019-10-14 14:03:41 -0500

    Sketched a new class called Student (with rating)

---
+++ inClass/Student.java
@@ -0,0 +1,17 @@
+package inClass;
+
+public class Student implements Comparable<Student>{
+	private String className;
+	private Integer rating;
+	
+	
+
+	@Override
+	public int compareTo(Student o) {
+		
+		return 0;
+	}
+	
+	
+	
+}
