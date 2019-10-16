8227d2a544de9f409bba312587e895210d0cc331
Joe <Joseph.Lucas-1@ou.edu>
2019-10-16 14:17:03 -0500

    StudentTest fix

--- inClass/Student.java
+++ inClass/Student.java
@@ -1,15 +1,29 @@
 package inClass;
 
-public class Student implements Comparable<Student>{
+public class Student extends Person implements Comparable<Student>{
 	private String className;
-	private Integer rating;
+	private Integer rating;	
 	
+	public Student() throws NameException {
+		super();
+		rating = 0;
+	}
+
+
 	
 
 	@Override
 	public int compareTo(Student o) {
 		
 		return 0;
+	}
+	
+
+	
+
+	public Integer getRating() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
---
+++ inClass/StudentTest.java
@@ -0,0 +1,28 @@
+package inClass;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTest {
+	
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+
+	@Test
+	public void testStudent() throws NameException {
+		Student s = new Student();
+		
+		Integer rating = s.getRating();
+		Integer expected
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
